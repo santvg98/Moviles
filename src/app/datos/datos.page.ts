5b9d6b6c35c74545b7ef099802782653dee4f3d2
Kayn2804 <143455711+Kayn2804@users.noreply.github.com>
2024-02-24 10:52:04 -0500

    taller de imagenes nacho lee 1

--- src/app/datos/datos.page.ts
+++ src/app/datos/datos.page.ts
@@ -1,69 +1,67 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-datos',
-  templateUrl: './datos.page.html',
+  templateUrl: './datos.page.html', 
   styleUrls: ['./datos.page.scss'],
+
 })
 export class DatosPage implements OnInit {
-  nombreSupervisor: string | null = ''
-
-  datos: Array<any> = []
-  show:{[key: number]: boolean} = {}
-  
-  date = new Date()
+  nombreSupervisor: string | null = '';
+  datos: Array<any> = [];
+  show: { [key: number]: boolean } = {};
+  date = new Date();
   fecha = this.date.toLocaleDateString('es-CO', {
     weekday: 'long',
     day: 'numeric',
     month: 'long',
   });
 
-  constructor(private activateRoute: ActivatedRoute) { }
+  constructor(private activateRoute: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
-    this.nombreSupervisor = this.activateRoute.snapshot.paramMap.get('nombre')
-    console.log("llego la cc: " + this.nombreSupervisor)
-    this.cajeros()
+    this.nombreSupervisor = this.activateRoute.snapshot.paramMap.get('nombre');
+    console.log('llego la cc: ' + this.nombreSupervisor);
+    this.cajeros();
   }
 
- 
-
-
-
-
-
-  cajeros(){
+  cajeros() {
     this.datos = [
       {
-        nombre: "Andres",
-        apellido: "pinilla",
+        nombre: 'Nacho',
+        apellido: 'lee',
         edad: 18,
-        caja: 1
+        caja: 1,
+        foto: '../../assets/img/nacho.jpg',
+        pagina: '../../assets/nacho.html',
       },
       {
-        nombre: "Luz Marina",
-        apellido: "Casañas",
-        edad: 47,
-        caja: 2
+        nombre: 'satoru ',
+        apellido: 'gojo',
+        foto: '../../assets/img/Gojo satoru cute♡.jpeg',
+        pagina:   '../../assets/gojo.html',
       },
       {
-        nombre: "Mariela",
-        apellido: "Hernandez",
-        edad: 87,
-        caja: 3
-      }
-    ]
+        nombre: 'Hu Tao',
+        apellido: '...',
+        foto: '../../assets/img/hu,tao.jpeg',
+        pagina: '../../assets/hutao.html',
+      },
+    ];
   }
-  
+
   onClick(i: number) {
-    if (this.show[i])
-    {
-      this.show[i]= false
-    } else
-    {
-      this.show[i]= true
+    if (this.show[i]) {
+      this.show[i] = false;
+    } else {
+      this.show[i] = true;
+    }
+  }
+
+  redireccionar(url: string) {
+    if (url) {
+      window.open(url, '_blank'); 
     }
   }
 }
